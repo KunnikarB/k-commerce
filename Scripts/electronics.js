17887462387516ade6391714b28a7d907a974282
kunnikarB <kunnikarboon@gmail.com>
2024-10-01 11:26:18 +0200

    created javaScript pages and add module type

---
+++ Scripts/electronics.js
@@ -0,0 +1,18 @@
+// Sort products based on selection
+document
+  .getElementById('js-sort-electronics')
+  .addEventListener('change', function () {
+    const sortValue = this.value;
+    let sortedProducts;
+
+    if (sortValue === 'price-asc') {
+      sortedProducts = electronics.sort((a, b) => a.price - b.price);
+    } else if (sortValue === 'price-desc') {
+      sortedProducts = electronics.sort((a, b) => b.price - a.price);
+    }
+
+    displayElectronics(sortedProducts);
+  });
+
+  // Display products on page load
+displayElectronics(electronics);
