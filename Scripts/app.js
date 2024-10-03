6ced889500d855c73939e2b3ac671be4cb303cec
kunnikarB <kunnikarboon@gmail.com>
2024-10-03 14:31:21 +0200

    Modifiled hamburger icon

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,19 @@
 const hamburer = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
+const burgerIcon = document.querySelector('.burger');
 
 if (hamburer) {
   hamburer.addEventListener('click', () => {
     navList.classList.toggle('open');
+    
+   if (burgerIcon.classList.contains('fa-bars')) {
+      burgerIcon.classList.remove('fa-bars');
+      burgerIcon.classList.add('fa-xmark');
+    } else {
+      burgerIcon.classList.remove('fa-xmark');
+      burgerIcon.classList.add('fa-bars');
+    }
+        
   });
 }
 
